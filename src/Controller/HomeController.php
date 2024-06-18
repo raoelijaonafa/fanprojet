c66cc3363033737a64318be3dbda126b839f749b
raoelijaonafa <jfanirytio@gmail.com>
2024-06-18 11:42:02 +0300

    twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,15 +1,20 @@
 <?php
-
-namespace App\Controller;
+    namespace App\Controller;
+    use  Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Response;
-
-class HomeCOntroller  extends AbstractController
-
-{   
+use Symfony\Component\HttpFoundation\Response;
+    class HomeController  extends AbstractController
+    {
+        
+          #[Route('/','home.index',methods:['GET'])]
+          
         public function index(): Response
+
         {
-            return $this->render('home.html.twig');
+           return $this-> render('home.html.twig');
         }
-}
+    }
+
+
+?>
